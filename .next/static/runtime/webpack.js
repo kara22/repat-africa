043deb88ac497cbca1551f3229003f5623f9c72d
kara Diaby <kdiaby.pro@gmail.com>
2020-01-03 11:52:45 +0100

    fix css problem to display landing page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c82c78f7bdfe2afc22cf";
+/******/ 	var hotCurrentHash = "0e1f9d33e83127de42c5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
